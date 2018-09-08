07ec7c65e1512d6ea523a0cbf080b8247e589b5a
Marek Vajgl <engin@seznam.cz>
2018-09-09 00:28:15 +0200

    Added "coalesce()" function

--- src/eng/eSystem/utilites/FunctionShortcuts.java
+++ src/eng/eSystem/utilites/FunctionShortcuts.java
@@ -5,7 +5,16 @@ public class FunctionShortcuts {
   private FunctionShortcuts() {
   }
 
-  public static String sf (String format, Object ... params){
+  public static String sf(String format, Object... params) {
     return String.format(format, params);
+  }
+
+  public static <T> T coalesce(T... items) {
+    T ret;
+    if (items == null) return null;
+    for (int i = 0; i < items.length; i++) {
+      if (items[i] != null) return items[i];
+    }
+    return null;
   }
 }
