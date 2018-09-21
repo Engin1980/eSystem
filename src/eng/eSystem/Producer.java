cc7d531781af080efa77d0ea11e829e0caa1acff
Engin1980 <engin@seznam.cz>
2018-09-21 15:09:55 +0200

    Added Producer interface and extended Validators.

---
+++ src/eng/eSystem/Producer.java
@@ -0,0 +1,5 @@
+package eng.eSystem;
+
+public interface Producer <T> {
+  T produce();
+}
--- src/eng/eSystem/validation/Validator.java
+++ src/eng/eSystem/validation/Validator.java
@@ -1,5 +1,10 @@
 package eng.eSystem.validation;
 
+import eng.eSystem.Action;
+import eng.eSystem.Producer;
+
+import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -8,7 +13,7 @@ import static eng.eSystem.utilites.FunctionShortcuts.sf;
 public class Validator {
   public static void isNotNull(Object value){
     if (value == null)
-      throw new ValidationException("Value is null.");
+      throw new ValidationException("Validation: Value is null.");
   }
 
   public static void matchPattern(String text, String pattern){
@@ -19,6 +24,16 @@ public class Validator {
   public static void matchPattern(String text, Pattern pattern){
     Matcher m = pattern.matcher(text);
     if (m.find() == false)
-      throw new ValidationException(sf("Value '%s' does not match pattern '%s'.", text, pattern.toString()));
+      throw new ValidationException(sf("Validation: Value '%s' does not match pattern '%s'.", text, pattern.toString()));
+  }
+
+  public static <T> void check(Producer<Boolean> check){
+    if (check.produce() == false)
+      throw new ValidationException("Validation: Value check failed.");
+  }
+
+  public static void check(boolean value){
+    if (value == false)
+      throw new ValidationException("Validation: Value check failed.");
   }
 }
