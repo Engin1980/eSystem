a97206f47b2960d30105814fbc734c3e637d16f0
Engin1980 <engin@seznam.cz>
2018-10-18 15:36:38 +0200

    ListBoxExtender now has function to detect mouse click.

--- src/eng/eSystem/swing/extenders/ListBoxExtender.java
+++ src/eng/eSystem/swing/extenders/ListBoxExtender.java
@@ -1,38 +1,72 @@
 package eng.eSystem.swing.extenders;
 
 import eng.eSystem.collections.*;
+import eng.eSystem.events.Event;
 import eng.eSystem.events.EventSimple;
 import eng.eSystem.utilites.StringUtils;
 
 import javax.swing.*;
 import javax.swing.event.ListSelectionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.function.Predicate;
 import java.util.regex.Pattern;
 
 public class ListBoxExtender<T> extends WithModelExtender<T, javax.swing.JList> {
 
+  public static class MouseClickEventArgs<T> {
+    private int count;
+    private T item;
+    private int button;
+
+    public MouseClickEventArgs(T item, int count) {
+      this.item = item;
+      this.count = count;
+    }
+
+    public T getItem() {
+      return item;
+    }
+
+    public int getCount() {
+      return count;
+    }
+  }
   protected final JList control;
   protected final IList<BoxItem<T>> items = new EDistinctList<>(EDistinctList.Behavior.skip);
   protected final DefaultListModel<BoxItem<T>> model = new DefaultListModel<>();
   private final EventSimple<ListBoxExtender> onSelectionChanged = new EventSimple<>(this);
-
-  public EventSimple<ListBoxExtender> getOnSelectionChanged() {
-    return onSelectionChanged;
-  }
+  private final Event<ListBoxExtender, MouseClickEventArgs<T>> getItemClick = new Event<>(this);
 
   public ListBoxExtender(JList control) {
     this.control = control;
     this.control.setModel(this.model);
     this.control.addListSelectionListener(this::control_selectionChanged);
-  }
 
-  private void control_selectionChanged(ListSelectionEvent listSelectionEvent) {
-    if (listSelectionEvent.getValueIsAdjusting() == false)
-      this.onSelectionChanged.raise();
+    this.control.addMouseListener(new MouseAdapter() {
+      public void mouseClicked(MouseEvent evt) {
+        JList list = (JList) evt.getSource();
+        int index = list.locationToIndex(evt.getPoint());
+        T item = null;
+        if (index >= 0 && index < ListBoxExtender.this.model.size())
+          item = ListBoxExtender.this.model.get(index).value;
+
+        MouseClickEventArgs<T> ea = new MouseClickEventArgs<>(item, evt.getClickCount());
+        ListBoxExtender.this.getItemClick.raise(ea);
+      }
+    });
   }
 
   public ListBoxExtender() {
     this(new JList());
+  }
+
+  public EventSimple<ListBoxExtender> getOnSelectionChanged() {
+    return onSelectionChanged;
+  }
+
+  public Event<ListBoxExtender, MouseClickEventArgs<T>> getGetItemClick() {
+    return getItemClick;
   }
 
   @Override
@@ -46,58 +80,6 @@ public class ListBoxExtender<T> extends WithModelExtender<T, javax.swing.JList> 
     updateByFilter(null);
   }
 
-  public void setFilter(String regex) {
-    if (StringUtils.isNullOrEmpty(regex))
-      updateByFilter(null);
-    else
-      updateByFilter(Pattern.compile(regex, Pattern.CASE_INSENSITIVE));
-  }
-
-  public void clearFilter(){
-    this.setFilter(null);
-  }
-
-  public void ensureFirstVisible(){
-    if (model.getSize() > 0)
-    this.ensureVisible(0);
-  }
-
-  public void ensureLastVisible(){
-    int index = model.getSize()-1;
-    if (index > -1)
-      this.ensureVisible(index);
-  }
-
-  public void ensureVisible(int index){
-    this.getControl().ensureIndexIsVisible(index);
-  }
-
-  public void ensureItemVisible(T item){
-    int index = getIndexOfItem(item);
-    if (index > -1)
-    this.getControl().ensureIndexIsVisible(index);
-  }
-
-  public void ensureLabelVisible(String label){
-    int index = getIndexOfLabel(label);
-    if (index > -1)
-      this.getControl().ensureIndexIsVisible(index);
-  }
-
-  private void updateByFilter(Pattern p) {
-    IReadOnlySet<T> currentSelected = this.getSelectedItems();
-    model.clear();
-    Predicate<String> testPredicate;
-    if (p != null)
-      testPredicate = p.asPredicate();
-    else
-      testPredicate = q -> true;
-    IList<BoxItem<T>> shownItems = items
-        .where(q -> testPredicate.test(q.label));
-    shownItems.forEach(q -> model.addElement(q));
-    this.setSelectedItems(currentSelected);
-  }
-
   @Override
   public IReadOnlySet<T> getItems() {
     ISet<T> ret = new ESet<>();
@@ -108,6 +90,44 @@ public class ListBoxExtender<T> extends WithModelExtender<T, javax.swing.JList> 
     return ret;
   }
 
+  public void setFilter(String regex) {
+    if (StringUtils.isNullOrEmpty(regex))
+      updateByFilter(null);
+    else
+      updateByFilter(Pattern.compile(regex, Pattern.CASE_INSENSITIVE));
+  }
+
+  public void clearFilter() {
+    this.setFilter(null);
+  }
+
+  public void ensureFirstVisible() {
+    if (model.getSize() > 0)
+      this.ensureVisible(0);
+  }
+
+  public void ensureLastVisible() {
+    int index = model.getSize() - 1;
+    if (index > -1)
+      this.ensureVisible(index);
+  }
+
+  public void ensureVisible(int index) {
+    this.getControl().ensureIndexIsVisible(index);
+  }
+
+  public void ensureItemVisible(T item) {
+    int index = getIndexOfItem(item);
+    if (index > -1)
+      this.getControl().ensureIndexIsVisible(index);
+  }
+
+  public void ensureLabelVisible(String label) {
+    int index = getIndexOfLabel(label);
+    if (index > -1)
+      this.getControl().ensureIndexIsVisible(index);
+  }
+
   public IReadOnlySet<T> getSelectedItems() {
     ISet<T> ret = new ESet<>();
     for (int selectedIndex : control.getSelectedIndices()) {
@@ -115,6 +135,23 @@ public class ListBoxExtender<T> extends WithModelExtender<T, javax.swing.JList> 
       ret.add(t.value);
     }
     return ret;
+  }
+
+  public void setSelectedItems(Iterable<T> items) {
+    IList<Integer> tmp = new EList<>();
+
+    for (T item : items) {
+      int index = getIndexOfItem(item);
+      if (index != -1)
+        tmp.add(index);
+    }
+
+    int[] arr = new int[tmp.size()];
+    for (int i = 0; i < tmp.size(); i++) {
+      arr[i] = tmp.get(i);
+    }
+
+    this.control.setSelectedIndices(arr);
   }
 
   public void setSelectedIndex(int index) {
@@ -160,21 +197,35 @@ public class ListBoxExtender<T> extends WithModelExtender<T, javax.swing.JList> 
     this.setSelectedItems(lst);
   }
 
-  public void setSelectedItems(Iterable<T> items) {
-    IList<Integer> tmp = new EList<>();
-
-    for (T item : items) {
-      int index = getIndexOfItem(item);
-      if (index != -1)
-        tmp.add(index);
+  public void selectAll() {
+    int[] arr = new int[this.model.size()];
+    for (int i = 0; i < arr.length; i++) {
+      arr[i] = i;
     }
-
-    int[] arr = new int[tmp.size()];
-    for (int i = 0; i < tmp.size(); i++) {
-      arr[i] = tmp.get(i);
-    }
-
     this.control.setSelectedIndices(arr);
+  }
+
+  public void selectNone() {
+    this.control.setSelectedIndices(new int[0]);
+  }
+
+  private void control_selectionChanged(ListSelectionEvent listSelectionEvent) {
+    if (listSelectionEvent.getValueIsAdjusting() == false)
+      this.onSelectionChanged.raise();
+  }
+
+  private void updateByFilter(Pattern p) {
+    IReadOnlySet<T> currentSelected = this.getSelectedItems();
+    model.clear();
+    Predicate<String> testPredicate;
+    if (p != null)
+      testPredicate = p.asPredicate();
+    else
+      testPredicate = q -> true;
+    IList<BoxItem<T>> shownItems = items
+        .where(q -> testPredicate.test(q.label));
+    shownItems.forEach(q -> model.addElement(q));
+    this.setSelectedItems(currentSelected);
   }
 
   protected int getIndexOfItem(T item) {
@@ -199,18 +250,6 @@ public class ListBoxExtender<T> extends WithModelExtender<T, javax.swing.JList> 
       }
     }
     return ret;
-  }
-
-  public void selectAll() {
-    int[] arr = new int[this.model.size()];
-    for (int i = 0; i < arr.length; i++) {
-      arr[i] = i;
-    }
-    this.control.setSelectedIndices(arr);
-  }
-
-  public void selectNone() {
-    this.control.setSelectedIndices(new int[0]);
   }
 }
 
