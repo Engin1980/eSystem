36a014d2e2f3b16e372313dc371a9680ccb62453
Engin1980 <engin@seznam.cz>
2020-06-04 23:44:16 +0200

    Updated multi-cause exception.

---
+++ src/eng/eSystem/exceptions/EMultiCauseException.java
@@ -0,0 +1,43 @@
+package eng.eSystem.exceptions;
+
+import eng.eSystem.EStringBuilder;
+
+import static eng.eSystem.utilites.FunctionShortcuts.sf;
+
+public class EMultiCauseException extends Exception {
+  private static Throwable getUniversalCause(int numberOfCauses) {
+    return new Exception(sf(
+        "There are %d causes of this exception. Check 'getCauses()' method of this exception.",
+        numberOfCauses));
+  }
+  private Throwable[] causes;
+
+  public EMultiCauseException(String message, Throwable[] causes) {
+    super(message, getUniversalCause(causes.length));
+    this.causes = causes;
+  }
+
+  public EMultiCauseException(String message, Throwable cause) {
+    super(message, cause);
+    this.causes = new Throwable[1];
+    this.causes[0] = cause;
+  }
+
+  @Override
+  public synchronized Throwable getCause() {
+    return super.getCause();
+  }
+
+  public Throwable[] getCauses() {
+    return causes;
+  }
+
+  @Override
+  public String getMessage() {
+    EStringBuilder sb = new EStringBuilder();
+    sb.append(super.getMessage());
+    sb.append(" Inner causes: ");
+    sb.appendItems(causes, "; ");
+    return sb.toString();
+  }
+}
