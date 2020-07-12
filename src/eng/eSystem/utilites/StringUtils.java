b77e19bf57fd9a881d9b4baebe99b6bdbc1bdc21
Marek Vajgl <engin@seznam.cz>
2020-07-13 01:23:51 +0200

    Added join method to StringUtils.

--- src/eng/eSystem/utilites/StringUtils.java
+++ src/eng/eSystem/utilites/StringUtils.java
@@ -1,27 +1,19 @@
 package eng.eSystem.utilites;
 
+import eng.eSystem.EStringBuilder;
+import eng.eSystem.collections.EList;
+import eng.eSystem.collections.IList;
+
 public class StringUtils {
-
-  public static boolean isNullOrEmpty(String value){
-    boolean ret = value == null || value.length() == 0;
-    return ret;
-  }
-
-  public static boolean isNullOrWhitespace(String value){
-    boolean ret = value == null || value.trim().length() == 0;
-    return ret;
-  }
-
-  private StringUtils() {
-  }
 
   /**
    * Gets beginning of the text until the delimiter is found.
-   * @param text Input text
+   *
+   * @param text      Input text
    * @param delimiter Delimiter value
    * @return Beginning of the string until delimiter, or the whole input text if delimiter was not found.
    */
-  public static String getUntil(String text, String delimiter){
+  public static String getUntil(String text, String delimiter) {
     String ret;
     int index = text.indexOf(delimiter);
     if (index <= 0)
@@ -30,4 +22,30 @@ public class StringUtils {
       ret = text.substring(0, index);
     return ret;
   }
+
+  public static boolean isNullOrEmpty(String value) {
+    boolean ret = value == null || value.length() == 0;
+    return ret;
+  }
+
+  public static boolean isNullOrWhitespace(String value) {
+    boolean ret = value == null || value.trim().length() == 0;
+    return ret;
+  }
+
+  public static String join(String delimiter, String... items) {
+    IList<String> lst = new EList<>(items);
+    return StringUtils.join(delimiter, lst);
+  }
+
+  public static String join(String delimiter, Iterable<String> items) {
+    EStringBuilder sb = new EStringBuilder();
+    sb.appendItems(items, delimiter);
+    return sb.toString();
+  }
+
+  private StringUtils() {
+  }
+
+
 }
