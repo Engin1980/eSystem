f786b249cab4d1d121f6c01e46559500f4dccabe
Marek Vajgl <engin@seznam.cz>
2020-07-25 14:09:23 +0200

    Added class comparer.

--- src/eng/eSystem/utilites/ObjectUtils.java
+++ src/eng/eSystem/utilites/ObjectUtils.java
@@ -2,7 +2,46 @@ package eng.eSystem.utilites;
 
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Comparator;
+
 public class ObjectUtils {
+
+  /**
+   * Orders classes by their inheritance.
+   *
+   * It is based on Class.isAssignableFrom() method.
+   */
+  public static class TypeOrderComparer implements Comparator<Class<?>> {
+
+    public enum Direction {
+      ascending,
+      descending;
+    }
+
+    private final Direction direction;
+
+    public TypeOrderComparer(Direction direction) {
+      this.direction = direction;
+    }
+
+    public TypeOrderComparer() {
+      this(Direction.ascending);
+    }
+
+    @Override
+    public int compare(Class<?> a, Class<?> b) {
+      int ret;
+      if (a.equals(b))
+        ret = 0;
+      else if (a.isAssignableFrom(b))
+        ret = -1;
+      else
+        ret = 1;
+      if (direction == Direction.descending)
+        ret = -ret;
+      return ret;
+    }
+  }
 
   public static boolean equals(@Nullable Object a, @Nullable Object b) {
     boolean ret;
