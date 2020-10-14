3fbeb4ad52eaff57e008a72c829cbc155ed6fc6a
Marek Vajgl <engin@seznam.cz>
2020-10-14 23:46:10 +0200

    Added cache utility classes.

---
+++ src/eng/eSystem/utilites/CacheUsingProducer.java
@@ -0,0 +1,31 @@
+package eng.eSystem.utilites;
+
+import eng.eSystem.functionalInterfaces.Producer;
+import eng.eSystem.validation.EAssert;
+
+public class CacheUsingProducer<T> {
+  private final Producer<T> instanceProducer;
+  private boolean obtained;
+  private T cachedValue = null;
+
+  public CacheUsingProducer(Producer<T> instanceProducer) {
+    EAssert.Argument.isNotNull(instanceProducer, "instanceProducer");
+    this.instanceProducer = instanceProducer;
+    this.obtained = false;
+  }
+
+  public void clear() {
+    this.cachedValue = null; this.obtained = false;
+  }
+
+  public T get() {
+    if (!obtained)
+      obtainInstance();
+    return cachedValue;
+  }
+
+  protected void obtainInstance() {
+    this.cachedValue = instanceProducer.produce();
+    this.obtained = true;
+  }
+}
--- src/eng/eSystem/utilites/CacheUsingSelector.java
+++ src/eng/eSystem/utilites/CacheUsingSelector.java
@@ -1,30 +1,28 @@
 package eng.eSystem.utilites;
 
-import eng.eSystem.EMath;
 import eng.eSystem.collections.EMap;
 import eng.eSystem.collections.IMap;
-import eng.eSystem.functionalInterfaces.Producer;
 import eng.eSystem.functionalInterfaces.Selector;
 import eng.eSystem.validation.EAssert;
 
-public class Cache<Tkey, Ttype> {
+public class CacheUsingSelector<Tkey, Ttype> {
   private final Selector<Tkey, Ttype> instanceProducer;
 
   private final IMap<Tkey, Ttype> cache = new EMap<>();
 
-  public Cache(Selector<Tkey, Ttype> instanceProducer) {
+  public CacheUsingSelector(Selector<Tkey, Ttype> instanceProducer) {
     EAssert.Argument.isNotNull(instanceProducer, "instanceProducer");
     this.instanceProducer = instanceProducer;
+  }
+
+  public void clear() {
+    this.cache.clear();
   }
 
   public Ttype get(Tkey key) {
     if (!cache.containsKey(key))
       obtainInstanceForKey(key);
     return cache.get(key);
-  }
-
-  public void clear() {
-    this.cache.clear();
   }
 
   protected void obtainInstanceForKey(Tkey key) {
